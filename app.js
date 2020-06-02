0bb0a2c5d4804a871ec8224fab6164e03cc72e26
scaraclette <gustiscarlett@gmail.com>
2020-06-02 11:32:17 -0700

    added client side

--- app.js
+++ app.js
@@ -17,4 +17,14 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
+if (process.env.NODE_ENV === 'production') {
+    // Serve any static files
+    app.use(express.static(path.join(__dirname, 'client/build')));
+
+    // Hanlde react routing, return all requests to React app
+    app.get('*', function(req, res) {
+        res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+    });
+}
+
 module.exports = app;
