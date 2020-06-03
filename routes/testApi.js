202e0afb836c4b1b2dbef72667496c10c8af1792
scaraclette <gustiscarlett@gmail.com>
2020-06-03 13:59:23 -0700

    successful deployment

--- routes/testApi.js
+++ routes/testApi.js
@@ -1,26 +1,14 @@
 var express = require('express');
 var router = express.Router();
-
-// const bodyParser = require('body-parser');
-// const cors = require('cors');
 const { pool } = require('../config');
 
 /* GET example API */
 router.get('/cat', function(req, res) {
-    console.log(pool);
-    console.log(process.env.NODE_ENV);
     pool.query('SELECT * FROM cats', (error, results) => {
         if (error) {
             throw error;
-            // res.status(404).json({'nope':'nothing'});
         } 
-        // else {
-        //     // console.log(process.env.NODE_ENV);
-        //     // console.log('heyyy')
-        //     res.status(200).json(results.rows);
-        // } 
         res.status(200).json(results.rows);
-        // pool.end();
     });
 });
 
