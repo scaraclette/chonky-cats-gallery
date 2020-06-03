b8059aec6c3b6d7565bee7fa8391739b4b31848b
scaraclette <gustiscarlett@gmail.com>
2020-06-03 13:50:04 -0700

    changed error code

--- routes/testApi.js
+++ routes/testApi.js
@@ -11,13 +11,15 @@ router.get('/cat', function(req, res) {
     console.log(process.env.NODE_ENV);
     pool.query('SELECT * FROM cats', (error, results) => {
         if (error) {
-            res.send(pool);
+            throw error;
             // res.status(404).json({'nope':'nothing'});
-        } else {
-            // console.log(process.env.NODE_ENV);
-            // console.log('heyyy')
-            res.status(200).json(results.rows);
         } 
+        // else {
+        //     // console.log(process.env.NODE_ENV);
+        //     // console.log('heyyy')
+        //     res.status(200).json(results.rows);
+        // } 
+        res.status(200).json(results.rows);
         pool.end();
     });
 });
